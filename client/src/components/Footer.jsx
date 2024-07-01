3776334dc7557f0d08a81e7d1e6a1e5d86af0f41
nischay-2 <nischay2@illinois.edu>
2024-07-01 18:08:06 -0500

    Created Footer

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,102 @@
+import { Footer } from "flowbite-react";
+import { Link } from "react-router-dom";
+import {
+  BsFacebook,
+  BsInstagram,
+  BsGithub,
+  BsTwitter,
+  BsDribbble,
+} from "react-icons/bs";
+import React from "react";
+
+export default function FooterComponent() {
+  return (
+    <Footer container className="border border-t-8 border-teal-500">
+      <div className="w-full max-w-7xl mx-auto">
+        <div className="grid w-full justify-between sm:flex md:grid-cols-1">
+          <div className="mt-5">
+            <Link
+              to="/"
+              className="self-center whitespace-nowrap text-lg
+      sm:text-xl font-semibold dark:text-white"
+            >
+              <span className="px-2 py-1 bg-gradient-to-r from-blue-500 via-indigo-500 to-purple-500 rounded-lg text-white">
+                BrewLab's
+              </span>
+              Blog
+            </Link>
+          </div>
+          <div className="grid grid-cols-2 gap-8 mt-4 sm:grid-cols-3 sm:gap-6">
+            <div>
+              <Footer.Title title="About" />
+              <Footer.LinkGroup col>
+                <Footer.Link href="#" target="_blank" rel="noopener norefrrer">
+                  Lorem Ipsum
+                </Footer.Link>
+                <Footer.Link
+                  href="https://www.google.com"
+                  target="_blank"
+                  rel="noopener norefrrer"
+                >
+                  Blog
+                </Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+            <div>
+              <Footer.Title title="Follow Us" />
+              <Footer.LinkGroup col>
+                <Footer.Link
+                  href="https://github.com/nischay-singh"
+                  target="_blank"
+                  rel="noopener norefrrer"
+                >
+                  Github
+                </Footer.Link>
+                <Footer.Link
+                  href="https://www.linkedin.com/in/nischay-singh/"
+                  target="_blank"
+                  rel="noopener norefrrer"
+                >
+                  LinkedIn
+                </Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+            <div>
+              <Footer.Title title="Legal" />
+              <Footer.LinkGroup col>
+                <Footer.Link href="https://github.com/nischay-singh">
+                  Privacy Policy
+                </Footer.Link>
+                <Footer.Link
+                  href="https://www.linkedin.com/in/nischay-singh/"
+                  target="_blank"
+                  rel="noopener norefrrer"
+                >
+                  Terms & Conditions
+                </Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+          </div>
+        </div>
+        <Footer.Divider />
+        <div className="w-full sm:flex sm:items-center sm:justify-between">
+          <Footer.Copyright
+            href="#"
+            by="Nischay's Website"
+            year={new Date().getFullYear()}
+          />
+          <div className="flex gap-6 sm:mt-0 mt-4 sm:justify-center">
+            <Footer.Icon href="#" icon={BsFacebook} />
+            <Footer.Icon href="#" icon={BsDribbble} />
+            <Footer.Icon href="#" icon={BsInstagram} />
+            <Footer.Icon
+              href="https://github.com/nischay-singh"
+              icon={BsGithub}
+            />
+            <Footer.Icon href="#" icon={BsTwitter} />
+          </div>
+        </div>
+      </div>
+    </Footer>
+  );
+}
